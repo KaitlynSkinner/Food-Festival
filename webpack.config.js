eb2a81ab4711f060d53bef6ad7c0b55badf77907
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-05-17 14:03:43 -0400

    added code and image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,53 @@
 // Require path module
 const path = require("path");
 const webpack = require("webpack");
+// Require webpack-bundle-analyzer plugin
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 module.exports = {
     // provide webpack with properties: entry, output, and mode
 
     // entry point is the root of the bundle and the beginning of the dependency graph (Note: give it the relative path to the client's code)
-    entry: './assets/js/script.js',
+    entry: {
+      app: "./assets/js/script.js",
+      events: "./assets/js/events.js",
+      schedule: "./assets/js/schedule.js",
+      tickets: "./assets/js/tickets.js"
+    },
 
     // output takes the bundled code, and outputs the bundle to a folder specified below
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + "dist",
+        filename: "[name].bundle.js"
+    },
+
+    // will identify the type of files to pre-process using the test property to find a regular expression, or regex
+    module: {
+      rules: [
+        {
+          // will process any image file with the file extension of .jpg
+          test: /\.jpg$/i,
+          // implement loader 
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                esModule: false,
+                name (file) {
+                  return "[path][name].[ext]"
+                },
+                publicPath: function(url) {
+                  return url.replace("../", "/assets/")
+                }
+              }
+            },
+            {
+              loader: 'image-webpack-loader'
+            }
+          ]
+        }
+      ]
     },
 
     // providePlugin plugin to define the $ and jQuery variables 
@@ -21,6 +56,9 @@ module.exports = {
           $: "jquery",
           jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
       ],
       
 
